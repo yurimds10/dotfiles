47dbddb98885a7501c7b3eeb003f9f8177d0d649
Yuri Mendes <coder73854@proton.me>
2025-06-22 20:46:51 -0300

    :bug: fix: switch icons in powermenu

--- home/config/rofi/powermenu/powermenu.sh
+++ home/config/rofi/powermenu/powermenu.sh
@@ -14,11 +14,11 @@ uptime="`uptime -p | sed -e 's/up //g'`"
 host=`hostname`
 
 # Options
-shutdown=''
-reboot=''
-lock=''
-suspend=''
-logout=''
+shutdown='󰤆 '
+reboot='󰁯 '
+lock='󰌾 '
+suspend=' '
+logout=' '
 yes=''
 no=''
 
